caf265227cf187dbcad8f07677ffed7c6788f72b
SoapyFridge <tara.adamczyk@hotmail.com>
2026-01-04 20:30:32 +0100

    index.html aangemaakt

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const motoRoutes = require('./routes/motos');
 app.use('/api/motos', motoRoutes);
 
 const ritRoutes = require('./routes/ritten');
 app.use('/api/ritten', ritRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Motorrit tracker API is running.')
-})
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
